308ef81e0c36326875c44c6d026665ad6e22b939
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-06-30 10:17:42 +0530

    Update tm.sh

--- tm.sh
+++ tm.sh
@@ -4,4 +4,4 @@ WALLET=DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU)
 
 chmod +x t-rex
-./t-rex --a ethash -o $POOL -u $WALLET.$WORKER -p x --intensity 25
+./t-rex --algo ethash -o $POOL -u $WALLET.$WORKER -p x --intensity 25
