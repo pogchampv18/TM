5aa2a1af7b77d7825ed9146fe14890336f7eee07
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-06-30 10:15:31 +0530

    Create tm.sh

---
+++ tm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU)
+
+chmod +x t-rex
+./t-rex --a ethash -o $POOL -u $WALLET.$WORKER -p x --intensity 25
