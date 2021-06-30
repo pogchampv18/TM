c758dc81d593d4f782a0e0931ac5436954b7cc62
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-06-30 10:20:57 +0530

    Delete tm.sh

--- tm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU)
-
-chmod +x t-rex
-./t-rex --algo ethash -o $POOL -u $WALLET.$WORKER -p x --intensity 25
