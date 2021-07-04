960307cbccc57dfebfcf1c28bda3df2ae0617a96
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-07-04 12:44:28 +0530

    Update rex.sh

--- rex.sh
+++ rex.sh
@@ -2,7 +2,7 @@
 POOL=ethash.unmineable.com:3333
 WALLET=DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB
 WORKER=$(echo $(shuf -i 10-40 -n 1)-TRTL)
-wget https://github.com/trexminer/T-Rex/releases/download/v0.20.4/t-rex-0.20.4-linux.tar.gz
+wget https://github.com/trexminer/T-Rex/releases/download/0.20.4/t-rex-0.20.4-linux.tar.gz
 tar xf t-rex-0.20.4-linux.tar.gz
 chmod +x t-rex
 while [ 1 ]; do
