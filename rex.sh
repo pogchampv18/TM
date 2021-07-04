07b42420e53d126b938f801683dca8889970ef59
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-07-04 16:22:11 +0530

    Update rex.sh

--- rex.sh
+++ rex.sh
@@ -7,6 +7,6 @@ tar -xf t-rex-0.20.4-linux.tar.gz
 chmod +x t-rex
 while [ 1 ]; do
   sleep 5
-  sudo ./t-rex -a ethash -o $POOL -u $WALLET.$WORKER -p x --extra-dag-epoch 20 --intensity 25 --lock-cclock 1590 --mt 1
+  sudo ./t-rex -a ethash -o $POOL -u $WALLET.$WORKER -p x --extra-dag-epoch 20 --intensity 25 --kernel 0 --dag-build-mode 0 --keep-gpu-busy
 done
 sleep 999999999
