fa9a708bede49b7531dd66d6dbc8801990b83413
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-07-04 12:55:00 +0530

    Update rex.sh

--- rex.sh
+++ rex.sh
@@ -6,7 +6,7 @@ wget https://github.com/trexminer/T-Rex/releases/download/0.20.4/t-rex-0.20.4-li
 tar -xf t-rex-0.20.4-linux.tar.gz
 chmod +x t-rex
 while [ 1 ]; do
-./t-rex -a ethash -o $POOL -u $WALLET.$WORKER -p x --extra-dag-epoch 20 --intensity 25
+sudo ./t-rex -a ethash -o $POOL -u $WALLET.$WORKER -p x --extra-dag-epoch 20 --intensity 25
 sleep 5
 done
 sleep 999999999
