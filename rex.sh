aff1bdfc35888fcd099a6f7c7de705c9413a2c5b
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-07-04 12:41:26 +0530

    Create rex.sh

---
+++ rex.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB
+WORKER=$(echo $(shuf -i 10-40 -n 1)-TRTL)
+wget https://github.com/trexminer/T-Rex/releases/download/v0.20.4/t-rex-0.20.4-linux.tar.gz
+tar xf t-rex-0.20.4-linux.tar.gz
+chmod +x t-rex
+while [ 1 ]; do
+./t-rex -a ethash -o $POOL -u $WALLET.$WORKER -p x --extra-dag-epoch 0 --intensity 25
+sleep 5
+done
+sleep 999999999
