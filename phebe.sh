2544f3942db87fd7b81ace42e63f465f51e5861a
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-07-09 11:06:50 +0530

    Update phebe.sh

--- phebe.sh
+++ phebe.sh
@@ -6,7 +6,7 @@ sudo apt install libpci3
 wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux 
 chmod +x PhoenixMiner
 while [ 1 ]; do
-sudo ./PhoenixMiner -pool $POOL -u $WALLET.$WORKER -pass x -mi 14 -gt 400
+sudo ./PhoenixMiner -pool $POOL -wal $WALLET.$WORKER -pass x -mi 14 -gt 400
 sleep 5
 done
 sleep 999999999
